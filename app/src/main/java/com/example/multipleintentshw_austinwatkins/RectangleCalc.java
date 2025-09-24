ea4091dfc36999ceab14abc1592ff839f12219c7
Hat-Master-PocketEdition <agwatkins04@gmail.com>
2025-09-24 16:57:57 -0400

    last push
    the work is done, just need to upload

--- app/src/main/java/com/example/multipleintentshw_austinwatkins/RectangleCalc.java
+++ app/src/main/java/com/example/multipleintentshw_austinwatkins/RectangleCalc.java
@@ -1,8 +1,12 @@
 package com.example.multipleintentshw_austinwatkins;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class RectangleCalc extends AppCompatActivity {
 
     Button RC_back;
+    EditText length;
+    EditText width;
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +31,11 @@ public class RectangleCalc extends AppCompatActivity {
             return insets;
         });
         RC_back = findViewById(R.id.rect_back_button);
+        length = findViewById(R.id.RectangleInputLength);
+        width = findViewById(R.id.RectangleInputWidth);
+        output = findViewById(R.id.RectangleOutput);
         setOnClickListenerBack();
+        textChangeListener();
     }
     public void setOnClickListenerBack()
     {
@@ -34,5 +45,44 @@ public class RectangleCalc extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void textChangeListener()
+    {
+        length.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(length.getText().toString(), width.getText().toString());
+            }
+        });
+        width.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(length.getText().toString(), width.getText().toString());
+            }
+        });
+    }
+    public void setText(String LS, String WS)
+    {
+        if (LS.isEmpty() || WS.isEmpty())
+        {
+            return;
+        }
+
+        double outL = Double.parseDouble(LS);
+        double outW = Double.parseDouble(WS);
+
+        double perimeter = (outL * 2) + (outW * 2);
+        double area = outL * outW;
+
+        output.setText("AREA = " + area + "\nPERIMETER = " + perimeter);
+
     }
 }
--- app/src/main/java/com/example/multipleintentshw_austinwatkins/TriangleCalc.java
+++ app/src/main/java/com/example/multipleintentshw_austinwatkins/TriangleCalc.java
@@ -2,8 +2,12 @@ package com.example.multipleintentshw_austinwatkins;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,11 @@ import androidx.core.view.WindowInsetsCompat;
 public class TriangleCalc extends AppCompatActivity {
 
     Button TR_back;
+    EditText base;
+    EditText side1;
+    EditText side2;
+    EditText height;
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +34,13 @@ public class TriangleCalc extends AppCompatActivity {
             return insets;
         });
         TR_back = findViewById(R.id.triangle_back_button);
+        base = findViewById(R.id.TriangleInputBase);
+        side1 = findViewById(R.id.TriangleInputSideOne);
+        side2 = findViewById(R.id.TriangleInputSideTwo);
+        height = findViewById(R.id.TriangleInputHeight);
+        output = findViewById(R.id.TriangleOutput);
         setOnClickListenerBack();
+        textChangeListener();
     }
     public void setOnClickListenerBack()
     {
@@ -35,5 +50,64 @@ public class TriangleCalc extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void textChangeListener()
+    {
+        base.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(base.getText().toString(), side1.getText().toString(), side2.getText().toString(), height.getText().toString());
+            }
+        });
+        side1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(base.getText().toString(), side1.getText().toString(), side2.getText().toString(), height.getText().toString());
+            }
+        });
+        side2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(base.getText().toString(), side1.getText().toString(), side2.getText().toString(), height.getText().toString());
+            }
+        });
+        height.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(base.getText().toString(), side1.getText().toString(), side2.getText().toString(), height.getText().toString());
+            }
+        });
+    }
+    public void setText(String w, String x, String y, String z)
+    {
+        if(w.isEmpty() || x.isEmpty() || y.isEmpty() || z.isEmpty())
+        {
+            return;
+        }
+        double outBase = Double.parseDouble(x);
+        double outSide1 = Double.parseDouble(x);
+        double outSide2 = Double.parseDouble(y);
+        double outHeight = Double.parseDouble(z);
+
+        double perimeter = outBase + outSide1 + outSide2;
+        double area = outBase + outHeight * 0.5;
+
+        output.setText("AREA = " + area + "\nPERIMETER = " + perimeter);
     }
 }
