6f9b94139c8cb2a07b05cebbf661fd423501c898
Hat-Master-PocketEdition <agwatkins04@gmail.com>
2025-09-24 16:40:37 -0400

    circle and square done
    last stretch

--- app/src/main/java/com/example/multipleintentshw_austinwatkins/CircleCalc.java
+++ app/src/main/java/com/example/multipleintentshw_austinwatkins/CircleCalc.java
@@ -1,8 +1,12 @@
 package com.example.multipleintentshw_austinwatkins;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CircleCalc extends AppCompatActivity {
     Button CR_back;
+    EditText radius;
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +29,10 @@ public class CircleCalc extends AppCompatActivity {
             return insets;
         });
         CR_back = findViewById(R.id.circle_back_button);
+        output = findViewById(R.id.CircleOutput);
+        radius = findViewById(R.id.CircleInputRadius);
         setOnClickListenerBack();
+        textChangeListener();
     }
     public void setOnClickListenerBack()
     {
@@ -33,5 +42,33 @@ public class CircleCalc extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void textChangeListener()
+    {
+        radius.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(radius.getText().toString());
+            }
+        });
+    }
+    public void setText(String x)
+    {
+        if(x.isEmpty())
+        {
+            return;
+        }
+        double radius = Double.parseDouble(x);
+        double pi = Math.PI;
+
+        double area = pi * Math.pow(radius, 2);
+
+        double perimeter = 2 * pi * radius;
+
+        output.setText("AREA = " + area + "\nPERIMETER = " + perimeter);
     }
 }
--- app/src/main/java/com/example/multipleintentshw_austinwatkins/SquareCalc.java
+++ app/src/main/java/com/example/multipleintentshw_austinwatkins/SquareCalc.java
@@ -2,8 +2,12 @@ package com.example.multipleintentshw_austinwatkins;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class SquareCalc extends AppCompatActivity {
 
     Button SQ_back;
+    EditText side;
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +31,10 @@ public class SquareCalc extends AppCompatActivity {
             return insets;
         });
         SQ_back = findViewById(R.id.square_back_button);
+        output = findViewById(R.id.SquareOutput);
+        side = findViewById(R.id.SquareInputOne);
         setOnClickListenerBack();
+        textChangeListener();
     }
     public void setOnClickListenerBack()
     {
@@ -35,5 +44,33 @@ public class SquareCalc extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void textChangeListener()
+    {
+        side.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setText(side.getText().toString());
+            }
+        });
+    }
+    public void setText(String x)
+    {
+        if(x.isEmpty())
+        {
+            return;
+        }
+        double y = Double.parseDouble(x);
+
+        double area = y * y;
+
+        double perimeter = y * 4;
+
+        output.setText("AREA = " + area + "\nPERIMETER = " + perimeter);
     }
 }
